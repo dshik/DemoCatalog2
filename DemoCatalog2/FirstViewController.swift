f5ab9bb20f6fe9d1c81528166cb47215fa540a51
dshik <dshik@mail.ru>
2020-09-10 11:27:36 +0300

    Начинаем делать демо. Есть TabBarController

---
+++ DemoCatalog2/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  DemoCatalog2
+//
+//  Created by Дмитрий Шикунов on 10.09.2020.
+//  Copyright © 2020 Дмитрий Шикунов. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
